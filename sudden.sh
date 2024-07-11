e732d45567abb0ebe1e7d430ee04103c738e2f33
thephunkmonk <andrewchoi1218@gmail.com>
2024-07-11 10:09:37 +0900

    edits to sudden.sh

--- sudden.sh
+++ sudden.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
 
-FILE=~/tmp/thetemple/_SUDDEN
+FILE=~/class_code/tmp/thetemple/_SUDDEN
 
 while true
 do
 
-if [ -e FILE ]; then
-echo "SUDDEN!!"
-sleep 3
+if [[ -f "$FILE" ]]; then
+   echo "check $FILE"
 else
-echo "not sudden ..."
-    sleep 3     
+   clear
+   date
 fi
+sleep 3
+
 done
+
+#if [ -e FILE ]; then
+#echo "SUDDEN!!"
+#sleep 3
+#else
+#echo "not sudden ..."
+#    sleep 3     
+#fi
+#done
 
 #echo "check"
 #sleep 3
