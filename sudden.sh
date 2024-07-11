36c31536fb3f2a355efbb6b61933a753bebde80e
thephunkmonk <andrewchoi1218@gmail.com>
2024-07-11 12:09:40 +0900

    edited sudden sh

--- sudden.sh
+++ sudden.sh
@@ -8,11 +8,11 @@ do
 DATE=$(date "+%Y%m%d %H:%M:%S")
 if [[ -f "$FILE" ]]; then
 #   echo "check $FILE"
-   echo "$DATE SUDDEN !!"
+   echo "$DATE,SUDDEN !!"
 else
    figlet Date@8
    echo $DATE
-   DATE=$(date "+%Y%m%d %H:%M:%S") 
+#   DATE=$(date "+%Y%m%d %H:%M:%S") 
 fi
 sleep 3
 
